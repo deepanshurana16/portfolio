603529a08ef7bb5dc606e148c9c2976d858aa51b
Deepanshu Rana <deepanshu.rana2021@vitstudent.ac.in>
2024-11-23 00:06:40 +0530

    base skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <div className="header"></div>
+      <div className="body"></div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
